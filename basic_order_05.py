e713336781d20c302983577e00579d670ab38146
mjgruano <mj.g.ruano@gmail.com>
2025-09-21 10:30:31 +0000

    new formula

--- basic_order_05.py
+++ basic_order_05.py
@@ -30,58 +30,26 @@ PRICE_JUICE = 3
 # - The function asks the user to input a number of items of type item_name
 # - The function returns an integer with the number of items
 
-def ask_number_items(item_name,current_bill):
+def ask_number_items(item_name, item_price, current_bill):
     items = input("How many " + item_name + " would you like?")
-    print ("You are ordering " + items + " " + item_name)
     
-    if items == "0":
-        current_bill = current_bill + int(item_number)*item_price
+    if items == "2":
+        print ("You are ordering " + items + " " + item_name)
+        current_bill = current_bill + int(items)*item_price
+
     elif items == "0":
-        current_bill = current_bill
+        print ("You are not ordering any " + item_name)
+        current_bill = current_bill 
+
     else:
         print("The input is not valid!")
-    
+
     return current_bill
 
+number_coffee = ask_number_items("coffee", PRICE_COFFEE, bill)
+number_croissant = ask_number_items("croissant", PRICE_CROISSANT, bill)
+number_juice = ask_number_items("juice", PRICE_JUICE, bill)
 
-number_coffee, bill = ask_number_items("coffee", bill)
-number_croissant, bill = ask_number_items("croissant", bill)
-numberr_juice, bill = ask_number_items("juice", bill)
-
-# Summarise the order
-# Function str transform a number into a text so we can add (+) elements of the same type 
-
-if order_coffee == "y" and order_croissant == "y" and order_juice == "y" :
-    print("You've ordered a coffee, a croissant and a juice!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "y" and order_croissant == "y" and order_juice == "n" :
-    print("You've ordered a coffee and a croissant!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "y" and order_croissant == "n" and order_juice == "y":
-    print("You've ordered a coffee and a juice!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "n" and order_croissant == "y" and order_juice == "y":
-    print("You've ordered a croissant and a juice!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "y" and order_croissant == "n" and order_juice == "n":
-    print("You've ordered a coffee!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "n" and order_croissant == "y" and order_juice == "n":
-    print("You've ordered a croissant!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-elif order_coffee == "n" and order_croissant == "n" and order_juice == "y":
-    print("You've ordered a juice!")
-    #print("The bill is " + str(bill) + " euros.")
-    #print("Thanks!")
-else:
-    print("You didn't order anything today!")
-    print("Hope you order something next time, thanks!")
 
 print ("The bill is " + str(bill) + " euros.")
 print ("Party of " + people + " people")
