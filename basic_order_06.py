53eec86f4b2e16e80a08543e82fecd3e1ed69823
mjgruano <mj.g.ruano@gmail.com>
2025-09-23 18:03:13 +0000

    def new functions

--- basic_order_06.py
+++ basic_order_06.py
@@ -16,11 +16,13 @@
 PRICE_COFFEE = 2
 PRICE_CROISSANT = 5
 
+"""
 print("Hello to the Python bar!")
 people = input("How many people?")
 print("Perfect. You can sit here")
+"""
 
-
+"""
 def ask_number(item_name):
     number_items = input("How many " + item_name + " would you like?")
 
@@ -39,8 +41,63 @@ number_croissant = ask_number("croissant")
 bill = int(number_coffee) * PRICE_COFFEE + int(number_croissant) * PRICE_CROISSANT
 
 print("The bill is " + str(bill))
+"""
+
+def get_price_coffee():
+    return PRICE_COFFEE
+
+def get_price_croissant():
+    return PRICE_CROISSANT
+
+# Use functions
+
+# Calculate the price of 2 coffees
+
+bill_coffees = 2 * get_price_coffee()
+
+print("The price of 2 coffees is " + str(bill_coffees))
+
+# Calculate the price of 4 croissants
+
+bill_croissants = 4 * get_price_croissant()
+
+print("The price of 4 croissants is " + str(bill_croissants)) 
+
+# Calculate the price of 3 coffees and 2 croissants
+
+bill_combo = 3 * get_price_coffee() + 2 * get_price_croissant()
+
+print("The price of 3 coffees and 2 croissants is " + str(bill_combo))
+
+def order_coffee():
+    number_coffee = input("How many coffees would you like?")
+    return number_coffee
+
+cost_coffee = PRICE_COFFEE * int(order_coffee())
+
+print("The total cost is " + str(cost_coffee))
+
+def get_price_n_coffees(n):
+    price = n * PRICE_COFFEE
+    return price
+
+# Calculate the price of 2 coffees
+
+bill_coffees = get_price_n_coffees(2) 
+
+print("The price of 2 coffees is " + str(bill_coffees))
+
+print("The price of 2 coffees is " + str(get_price_n_coffees(2)))
+
+print("The price of 2 coffees is " + str(2 * PRICE_COFFEE))
 
 # Create the function multiply(number_1, number_2) that calculates the product number_1 * number_2 and prints the result
 # Use the function to calculate 2*3 + 4*5 calling the function multiply to solve the multiplications
 
+def multiply(number_1, number_2):
+    return number_1 * number_2
 
+op1 = multiply(2, 3)
+op2 = multiply(4, 5)
+op3 = op1 + op2
+print("The result of 2*3 + 4*5 is " + str(op3))
